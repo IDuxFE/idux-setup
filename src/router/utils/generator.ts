33c09977d9731bc476be56cc39d538e3fbe97b6c
tuchg <24775733+tuchg@users.noreply.github.com>
2022-07-21 10:08:00 +0800

    fix: remove meaningless v-model (#5)

--- src/router/utils/generator.ts
+++ src/router/utils/generator.ts
@@ -1,1 +1,5 @@
-export const generateRoutes = (): void => {}
+export const generateRoutes = (): void => {
+  // 动态引入（chunk)
+  // const viewsModules = import.meta.glob('@/views/**/*.{vue,tsx}')
+  // console.log(viewsModules)
+}
